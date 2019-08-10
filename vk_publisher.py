5dbd91a89adfd1f0d811adf2d1c464569515c4aa
Pserga <psergal@gmail.com>
2019-08-11 00:52:15 +0300

    Combine download and posting
    * Realise random choice
    * Use pathlib for file operation
    * Transfer dict from load func to posting func

--- vk_publisher.py
+++ vk_publisher.py
@@ -1,15 +1,16 @@
 from dotenv import load_dotenv
-import os
+from os import getenv
 import requests
 import argparse
 import load_store as ls
-
+import random
+import pathlib
 
 
 def get_args():
     parser = argparse.ArgumentParser(description='Downloading comics from xkcd.com')
     parser.add_argument('--img_name', default='python',  help='Define image name default=python')
-    parser.add_argument('--img_id', default='353',  help='Define image id default=353')
+    parser.add_argument('--img_id',   help='Define image id if empty then random')
     parser.add_argument('--api', default='https://xkcd.com/', help=argparse.SUPPRESS)
     parser.add_argument('--headers', default={
         'User-Agent': 'curl',
@@ -19,7 +20,7 @@ def get_args():
     }, help=argparse.SUPPRESS)
     parser.add_argument('--img_dir', default='images',  help='Define image folder default=images')
     args = parser.parse_args()
-    args.api = f'{args.api}{args.img_id}/info.0.json'
+
     return args
 
 def get_comic():
@@ -28,14 +29,26 @@ def get_comic():
     :return:
     """
     args = get_args()
-    resp = requests.get(args.api, headers=args.headers)
+    if args.img_id is None:
+        url = f'{args.api}info.0.json'
+        resp = requests.get(url,  headers=args.headers)
+        if not resp.ok:
+            raise requests.exceptions.HTTPError(resp['error'])
+        comic = resp.json()
+        img_id = random.choice(range(comic.get('num')))
+        url = f'{args.api}{img_id}/info.0.json'
+    else:
+        img_id = args.img_id
+    url = f'{args.api}{img_id}/info.0.json'
+    resp = requests.get(url, headers=args.headers)
     if not resp.ok:
         raise requests.exceptions.HTTPError(resp['error'])
     comic = resp.json()
+
     img_links = [comic.get('img')]
     alt = comic.get('alt')
-    written_files = ls.load_n_store(img_links, args.img_name, args.img_dir)
-    print(alt)
+    written_files = ls.load_n_store(img_links, f'{args.img_name}_{img_id}', args.img_dir)[0]
+    return {'file': written_files, 'comments': alt, 'dir': args.img_dir}
 
 
 def vk_request(method, url, params, files):
@@ -50,14 +63,10 @@ def vk_request(method, url, params, files):
     return json_resp
 
 
-def vk_post_pic_onto_wall ():
+def vk_post_pic_onto_wall(comic_file):
     extended = 1
-    cur_dir = os.path.dirname(__file__)
-    image_path = os.path.join(cur_dir, 'images') # из аргументов
-    pics = os.listdir(image_path)
-    if len(pics) == 0:
-        quit()
-    pic_full_path = os.path.join(image_path, pics[0])
+    cur_dir = pathlib.PurePath(__file__).parent
+    img_path = cur_dir.joinpath(comic_file['dir'], comic_file['file'])
     version = '5.101'
     method = ['groups.get', 'photos.getWallUploadServer', 'photos.saveWallPhoto', 'wall.post']
     params = {'user_id': vk_user_id, 'extended': extended, 'access_token': vk_access_token, 'v': version}
@@ -71,7 +80,7 @@ def vk_post_pic_onto_wall ():
     vk_reponse = vk_request('get', url, params, None).get('response')
 
     upload_url = vk_reponse.get('upload_url')
-    with open(pic_full_path, 'rb') as file:
+    with open(img_path, 'rb') as file:
         files = {'photo': file}
         vk_reponse = vk_request('post', upload_url, None, files)
 
@@ -79,26 +88,27 @@ def vk_post_pic_onto_wall ():
     params.update({'photo': vk_reponse.get('photo')})
     params.update({'hash': vk_reponse.get('hash')})
     params.update({'user_id': vk_user_id})
-    params.update({'caption': 'Python comics'})
+    params.update({'caption': 'Random comics'})
     url = f'https://api.vk.com/method/{method[2]}' # SaveWallPhoto
     vk_reponse = vk_request('get', url, params, None).get('response')[0]
 
     vk_media_id = vk_reponse.get('id')
     vk_owner_id = vk_reponse.get('owner_id')
-    params.update({'message': 'Look at this Python comics!!!2222'})
+    params.update({'message': comic_file['comments']})
     params.update({'owner_id': f'-{group_id}'})
     params.update({'from_group': '1'})
     params.update({'attachments': f'photo{vk_owner_id}_{vk_media_id}'})
     del params['server'], params['photo'], params['hash'], params['caption'], params['user_id'], params['group_id']
     url = f'https://api.vk.com/method/{method[3]}'
     vk_reponse = vk_request('get', url, params, None)
+    del_path = pathlib.Path(img_path)
+    del_path.unlink()
     print(vk_reponse)
 
 
 if __name__ == '__main__':
     load_dotenv()
-    vk_access_token = os.getenv('VK_ACCESS_TOKEN')
-    vk_user_id = os.getenv('VK_ID')
-    # vk_app_id = os.getenv('VK_APP_ID')
-    # get_comic()
-    vk_post_pic_onto_wall()
+    vk_access_token = getenv('VK_ACCESS_TOKEN')
+    vk_user_id = getenv('VK_ID')
+    comic_file = get_comic()
+    vk_post_pic_onto_wall(comic_file)
